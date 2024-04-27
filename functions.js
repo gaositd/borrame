302ce47635675ca277b4a333ef0c11aa969e765d
Alvaro Gamez Chavez <gaositd@gmail.com>
2024-04-26 21:53:12 -0600

    trabando una app

--- functions.js
+++ functions.js
@@ -1,12 +1,38 @@
-function contador() {
-  let contador = 0;
-  // while(true) {
-  //   if(contador === 10000000) {
-  //     false;
-  //   } else {
-  //     contador ++;
-  //     document.getElementById("contador").innerHTML = `<span id='contador' onload='contador()'>${contador}</span>`;
+// const $ = selector => document.querySelector(selector);
+
+// const $contador = $('#contador');
+// const $button = $('#boton');
+
+// $button.addEventListener('click', () => {
+//   let i = 0;
+//   while(true){
+//     if(i  === 10000000){
+//       break;
+//     }
+    
+//     i++;
+//     $contador.textContent = i;
+    
+//   }
+//   $contador.textContent = parseInt($contador.textContent) + 1;
+// });
+
+const $ = selector => document.querySelector(selector);
+const $contador = $('#contador');
+const $button = $('#boton');
+
+$button.addEventListener('click', () => {
+  // let i = 0;
+  // while(true){
+  //   if(i  === 10000000){
+  //     break;
   //   }
+
+  //   i++;
+  //   $contador.textContent = i;
+
   // }
-  document.getElementById("contador").innerHTML = `<span id='contador' onload='contador()'>${contador}</span>`;
-}
+  // $contador.textContent = parseInt($contador.textContent) + 1;
+  const contador = +$button.innerText
+  $contador.textContent = contador + 1;
+});
--- js/functions.js
+++ js/functions.js
@@ -1,13 +1,52 @@
-function contador() {
-  let contador = 0;
-  // while(true) {
-  //   if(contador === 10000000) {
-  //     false;
-  //   } else {
-  //     contador ++;
-  //     document.getElementById("contador").innerHTML = `<span id='contador' onload='contador()'>${contador}</span>`;
-  //   }
-  // }
-  document.getElementById("contador").innerHTML = `<span id='contador' onload='contador()'>${contador}</span>`;
-  // https://www.youtube.com/watch?v=ir9yaSgbOdY minuto 30:15
-}
+// function contador() {
+//   let contador = 0;
+//   // while(true) {
+//   //   if(contador === 10000000) {
+//   //     false;
+//   //   } else {
+//   //     contador ++;
+//   //     document.getElementById("contador").innerHTML = `<span id='contador' onload='contador()'>${contador}</span>`;
+//   //   }
+//   // }
+//   document.getElementById("contador").innerHTML = `<span id='contador' onload='contador()'>${contador}</span>`;
+//   // https://www.youtube.com/watch?v=ir9yaSgbOdY minuto 30:15
+// }
+
+// const $ = selector => document.querySelector(selector);
+
+// const $contador = $('#contador');
+// const $button = $('#boton');
+
+// $button.addEventListener('click', () => {
+//   let i = 0;
+//   while(true){
+//     if(i  === 10000000){
+//       break;
+//     }
+    
+//     i++;
+//     $contador.textContent = i;
+    
+//   }
+//   $contador.textContent = parseInt($contador.textContent) + 1;
+// });
+
+const $ = selector => document.querySelector(selector);
+const $contador = $('#contador');
+const $button = $('#boton');
+
+$button.addEventListener('click', () => {
+  let i = 0;
+  while(true){
+    if(i  === 100000){
+      break;
+    }
+
+    i++;
+    $contador.textContent = i;
+
+    // $contador.textContent = parseInt($contador.textContent) + 1;
+  }
+  // const contador = +$contador.innerText
+  // $contador.textContent = contador + 1;
+});
